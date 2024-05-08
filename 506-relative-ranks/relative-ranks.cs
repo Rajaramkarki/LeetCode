4d486e9ffdf1d1f58c1e0a6ae57c48051419d328
Rajaram Karki <67723187+Rajaramkarki@users.noreply.github.com>
2024-05-08 13:31:40 +0545

    Time: 153 ms (28.37%) | Memory: 70.7 MB (34.75%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public string[] FindRelativeRanks(int[] score) {
+        int l = score.Length;
+        string[] result = new string[l];
+
+        Dictionary<int, int> myDict = new Dictionary<int, int>();
+
+        for(int i = 0; i < l; i++)
+        {
+            myDict[score[i]] = i;
+        }
+
+        Array.Sort(score,(x,y) => y.CompareTo(x));
+        
+        for(int i = 0; i< l; i++)
+        {
+            if(i == 0)
+                result[myDict[score[i]]] = "Gold Medal";
+
+            else if(i == 1)
+                result[myDict[score[i]]] = "Silver Medal";
+
+            else if(i == 2)
+                result[myDict[score[i]]] = "Bronze Medal";
+
+            else
+                result[myDict[score[i]]] = (i+1).ToString();
+        }
+
+        return result;
+
+    }
+}

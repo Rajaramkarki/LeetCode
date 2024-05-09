1b9aa1aee33c4f4050ceaa1a19bbaae47f118a23
Rajaram Karki <67723187+Rajaramkarki@users.noreply.github.com>
2024-05-09 17:38:51 +0545

    Time: 70 ms (50.00%) | Memory: 41.4 MB (17.86%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cs
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cs
@@ -1,10 +1,6 @@
 public class Solution {
     public int MaxProduct(int[] nums) {
         Array.Sort(nums, (x,y) => y.CompareTo(x));
-
-        int result = (nums[0] - 1) * (nums[1] - 1);
-
-        return result;
-        
+        return (nums[0] - 1) * (nums[1] - 1);
     }
 }
